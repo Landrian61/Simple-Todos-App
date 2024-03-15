4c7827d30b0834fc6f02914c67eb964238cf1993
Luswata Andrew <102017551+Landrian61@users.noreply.github.com>
2024-03-15 15:14:57 +0300

    Simple Todos App Javascript

--- server/server.js
+++ server/server.js
@@ -17,7 +17,8 @@ app.use(cors());
 app.use(bodyParser.json());
 
 // MongoDB connection
-mongoose.connect('mongodb://localhost:27017/todo-app', {
+mongoose.connect("mongodb+srv://landrian61:FL4glLmvlr1k3dpP@cluster0.uuysxqq.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
